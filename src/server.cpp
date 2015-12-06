a937d74d0084ff6e7b5e412caf393f1a571d95a0
Patrick Hurley <phurley@gmail.com>
2015-12-06 00:55:36 -0500

    Fixed issues with server class, now integrate into roboconfig

--- src/server.cpp
+++ src/server.cpp
@@ -1,55 +1,126 @@
+#include <fstream>
 #include "server.hpp"
 
-Server::Server(int p, const std::string& ap, const std::string& r, const std::string& c) :
+const struct mg_str Server::get_method = MG_STR("GET");
+const struct mg_str Server::post_method = MG_STR("POST");
+const struct mg_str Server::delele_method = MG_STR("DELETE");
+
+Server::Server(const std::string& p, const std::string& ap, const std::string& r, const std::string& c) :
   port(p), api_prefix(ap), root(r), config_path(c) {
   setup();
 }
 
 Server::Server(const Json::Value& config) {
+  setup(config);
 }
 
 Server::Server(const std::string fname) {
+  Json::Value server_config;
+  std::ifstream stream(fname, std::ifstream::binary);
+  stream >> server_config;
+
+  setup(server_config);
 }
 
 void Server::setup() {
   struct mg_connection *nc;
 
-  mg_mgr_init(&mgr, NULL);
+  mg_mgr_init(&mgr, this);
 
-  nc = mg_bind(&mgr, port.c_str(), ev_handler);
-  s_http_server_opts.document_root = root.c_str();
+  nc = mg_bind(&mgr, port.c_str(), raw_event_handler);
+  server_opts.document_root = root.c_str();
   mg_set_protocol_http_websocket(nc);
+}
+
+void Server::setup(const Json::Value& config) {
+  port = config.get("port", "8080").asString();
+  api_prefix = config.get("api_prefix", "/api").asString();
+  root = config.get("document_root", "public").asString();
+  config_path = config.get("config_directory", "config").asString();
+
+  setup();
 }
 
 Server::~Server() {
   mg_mgr_free(&mgr);
 }
 
+void Server::event_handler(struct mg_connection *nc, int ev, void *ev_data) {
+  switch (ev) {
+    case MG_EV_HTTP_REQUEST:
+      http_request(nc, ev, (struct http_message*) ev_data);
+      break;
+
+    case MG_EV_WEBSOCKET_HANDSHAKE_REQUEST:
+      websocket_request(nc, (struct http_message*) ev_data);
+      break;
+        
+    case MG_EV_WEBSOCKET_HANDSHAKE_DONE:
+      websocket_create(nc);
+      break;
+
+    case MG_EV_WEBSOCKET_FRAME:
+      websocket_frame(nc, (struct websocket_message*) ev_data); 
+      break;
+
+    case MG_EV_CLOSE:
+      websocket_close(nc); 
+      break;
+
+    case MG_EV_POLL:
+      idle(nc); 
+      break;
+  }
+}
+
+void Server::raw_event_handler(struct mg_connection *nc, int ev, void *ev_data) {
+  Server* me = (Server *) nc->mgr->user_data;
+  me->event_handler(nc, ev, ev_data);
+}
+
 void Server::poll(int delay) {
   mg_mgr_poll(&mgr, delay);
 }
 
-void Server::http_request() {
+void Server::http_request(struct mg_connection *nc, int, struct http_message *hm) {
+  if (is_equal(&hm->method, &get_method)) {
+    http_get(nc, hm);
+
+  } else if (is_equal(&hm->method, &post_method)) {
+    http_post(nc, hm);
+
+  } else if (is_equal(&hm->method, &delele_method)) {
+    http_delete(nc, hm);
+
+  }
 }
 
-void Server::http_get() {
+void Server::http_get(struct mg_connection *, struct http_message *) {
 }
 
-void Server::http_post() {
+void Server::http_post(struct mg_connection *, struct http_message *) {
 }
 
-void Server::http_delete() {
+void Server::http_delete(struct mg_connection *, struct http_message *) {
 }
 
-void Server::http_response() {
+void Server::websocket_request(struct mg_connection *, struct http_message *) {
 }
 
-void Server::websocket_create() {
+void Server::websocket_create(struct mg_connection *) {
 }
 
-void Server::websocket_frame() {
+void Server::websocket_frame(struct mg_connection *, struct websocket_message *) {
 }
 
-void Server::websocket_close() {
+void Server::websocket_close(struct mg_connection *) {
+}
+
+void Server::send_http_response(struct mg_connection *nc, const std::string& body, int code, const std::string& reason) {
+  mg_printf(nc, "HTTP/1.1 %d %s\r\nContent-Length: %d\r\n\r\n%s", code, reason.c_str(), (int) body.size(), body.c_str());
+}
+
+void Server::send_http_error(struct mg_connection *nc, int code, const std::string& reason) {
+  send_http_response(nc, "", code, reason);
 }
 
--- src/server.hpp
+++ src/server.hpp
@@ -1,36 +1,56 @@
 #ifndef SERVER_HPP__
 #define SERVER_HPP__
 
+#include <string>
+#include "json/json.h"
 #include "mongoose.h"
 
 class Server {
 public:
-  Server(int p=8080, const std::string& ap="/api", const std::string& r="public", const std::string& c="config");
+  Server(const std::string& p="8080", const std::string& ap="/api", const std::string& r="public", const std::string& c="config");
   Server(const Json::Value& config);
   Server(const std::string fname);
 
   virtual ~Server();
 
+  virtual void idle(struct mg_connection*) {}
   virtual void poll(int sleep = 100);
-  virtual void http_request();
-  virtual void http_get();
-  virtual void http_post();
-  virtual void http_delete();
-  virtual void http_response();
+  virtual void http_request(struct mg_connection *nc, int ev, struct http_message *hm);
+  virtual void http_get(struct mg_connection *nc, struct http_message *hm);
+  virtual void http_post(struct mg_connection *nc, struct http_message *hm);
+  virtual void http_delete(struct mg_connection *nc, struct http_message *hm);
  
-  virtual void websocket_create();
-  virtual void websocket_frame();
-  virtual void websocket_close();
+  virtual void websocket_request(struct mg_connection *nc, struct http_message *hm);
+  virtual void websocket_create(struct mg_connection *nc);
+  virtual void websocket_frame(struct mg_connection *nc, struct websocket_message *wm);
+  virtual void websocket_close(struct mg_connection *nc);
   
+  static bool is_equal(const struct mg_str *s1, const struct mg_str *s2) {
+    return s1->len == s2->len && memcmp(s1->p, s2->p, s2->len) == 0;
+  }
+
+  static const struct mg_str get_method;
+  static const struct mg_str post_method;
+  static const struct mg_str delele_method;
+
+  void send_http_response(struct mg_connection *nc, const std::string& body="", int code=200, const std::string& reason="");
+  void send_http_error(struct mg_connection *nc, int code, const std::string& reason="");
+
 protected:
   struct mg_mgr mgr;
+  struct mg_serve_http_opts server_opts;
+  virtual void event_handler(struct mg_connection *nc, int ev, void *ev_data);
 
   virtual void setup();
+  virtual void setup(const Json::Value& config);
 
-  int port;
+  std::string port;
   std::string api_prefix;
   std::string root;
   std::string config_path; 
+
+private:
+  static void raw_event_handler(struct mg_connection *nc, int ev, void *ev_data);
 };
 
 #endif
